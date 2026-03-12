f91ff5d2f4165a72d46124b5800664013098f17f
Jppcamilo <jpcamilopereira@gmail.com>
2026-03-12 20:59:19 -0300

    Adicionamos mais imports que serã utilizados no futuro e adicionamos novos arquivos

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './src/screens/HomeScreen';
+import ResultScreen from './src/screens/ResultScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+
+      <View style={styles.container}>
+        <HomeScreen />
+      <ResultScreen />
       <StatusBar style="auto" />
     </View>
   );
